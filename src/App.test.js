0d7b4677a9f840ffbd931d2fdd481773b38ec05a
Taylor Curry <currytay@gmail.com>
2022-02-21 00:22:02 +1100

    Create sample test cases

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import renderer from 'react-test-renderer';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('renders without crashing', () => {
+  shallow(<App />);
+});
+
+it('renders user handle', () => {
+  const profile = shallow(<App />);
+  const handle = <h1>@yourname</h1>;
+  expect(profile.contains(handle));
+});
+
+test('snapshot renders', () => {
+  const component = renderer.create(<App />);
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 });
