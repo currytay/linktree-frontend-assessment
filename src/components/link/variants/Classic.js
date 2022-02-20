2a0751ba0bb076217c7e241c26f9350c32328d25
Taylor Curry <currytay@gmail.com>
2022-02-20 16:25:41 +1100

    Style classic link and add functionality

--- src/components/link/variants/Classic.js
+++ src/components/link/variants/Classic.js
@@ -1,10 +1,39 @@
 import React from 'react';
 import styled from 'styled-components';
+import { invertedTheme, theme } from '../../../data/mock-theme';
 
-const Classic = () => {
+const Classic = ({ label, link, music, shows, variant }) => {
   return (
-    <p>Classic</p>
+    <ClassicLink href={link} target='_blank'>
+      <LinkText>{label}</LinkText>
+    </ClassicLink>
   );
 };
+
+const ClassicLink = styled.a`
+  width: inherit;
+  background-color: ${theme.bg};
+  color: ${theme.fg};
+  border: 2px solid ${theme.bg};
+  border-radius: .25rem;
+  padding: .75rem 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: .15s all ease;
+
+  :hover {
+    background-color: ${invertedTheme.bg};
+    color: ${invertedTheme.fg};
+  }
+`;
+
+const LinkText = styled.p`
+  width: inherit;
+  color: inherit;
+  line-height: 1.5rem;
+  margin: 0;
+  text-align: center;
+`;
 
 export default Classic;
