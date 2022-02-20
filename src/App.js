d87d344ec42b1142ae17a5bfc7ba2c0c4358ecef
Taylor Curry <currytay@gmail.com>
2022-02-20 14:43:50 +1100

    Set up page skeleton

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
 
-function App() {
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <ProfileContainer>
+
+      </ProfileContainer>
+    </AppContainer>
   );
-}
+};
+
+const AppContainer = styled.div`
+  width: 100%;
+`;
+
+const ProfileContainer = styled.div`
+  width: 100%;
+  max-width: 42.5rem;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 2rem 1.5rem;
+`;
 
 export default App;
